13d95955151204a0dc6b1441458eedcfdb24b441
greenrenge <greenrenge@gmail.com>
2020-12-09 14:57:03 +0700

    migrate

--- playground/index.js
+++ playground/index.js
@@ -1,24 +1,31 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import express from "express"
+import createLightship from "../src"
+
+const app = express()
+
 process.env.ROARR_LOG = true
 process.env.KUBERNETES_SERVICE_HOST = true
-const app = require("express")()
-const { create } = require("./index")
-const { createReadiness } = create({})
+
+const { createReadiness } = createLightship({ port: 13000 })
 const [one, two, three, four, five] = createReadiness(5)
 
-
 app.get("/:id", (req, res) => {
-  const id = req.params.id
+  const {
+    params: { id },
+  } = req
   const mapping = { "1": one, "2": two, "3": three, "4": four, "5": five }
   if (mapping[id]) mapping[id]()
   res.status(200).send(id)
 })
 
 app.post("/:id", (req, res) => {
-  const id = req.params.id
+  const {
+    params: { id },
+  } = req
   const mapping = { "1": one, "2": two, "3": three, "4": four, "5": five }
   if (mapping[id]) mapping[id].toNotReady()
   res.status(200).send(id)
 })
 
-app.listen(4040)
-
+app.listen(3000)
---
+++ src/index.js
@@ -0,0 +1,87 @@
+import * as Lightship from "lightship"
+
+const { createLightship: createLightShipInstance } = Lightship
+
+const DEFAULT_PORT = 13000
+
+const create = (
+  {
+    detectKubernetes,
+    port: customPort = DEFAULT_PORT,
+    signals,
+    terminate,
+    gracefulShutdownTimeout,
+    enableLog,
+    randomPortOnLocal,
+  } = { port: DEFAULT_PORT },
+) => {
+  if (enableLog) process.env.ROARR_LOG = true
+  const isOnLocal = !process.env.KUBERNETES_SERVICE_HOST
+
+  if (isOnLocal && !randomPortOnLocal) {
+    // mock k8s env to force lightship use config port
+    process.env.KUBERNETES_SERVICE_HOST = "kubernetes.default.svc.cluster.local"
+  }
+  const isTestENV = process.env.NODE_ENV === "test"
+  const randomPort =
+    ["true", true].includes(process.env.LIGHTSHIP_RANDOM_PORT) || isTestENV
+  const port = randomPort ? 0 : customPort // random port
+
+  const lightship = createLightShipInstance({
+    ...(detectKubernetes && { detectKubernetes }),
+    ...(port && { port }),
+    ...(signals && { signals }),
+    ...(terminate && { terminate }),
+    ...(gracefulShutdownTimeout && { gracefulShutdownTimeout }),
+  })
+  if (isTestENV) lightship.server.close()
+
+  let createdReadiness
+  const wrapLightship = {
+    lightship,
+    createReadiness: (number = 1) => {
+      if (createdReadiness) return createdReadiness
+      let n = number
+      const createDeduct = () => {
+        let isReady = false
+
+        const toNotReady = () => {
+          if (isReady) {
+            isReady = false
+            n += 1
+            lightship.signalNotReady()
+          }
+        }
+
+        const toReady = () => {
+          if (!isReady) {
+            isReady = true
+            n -= 1
+            if (n <= 0) {
+              lightship.signalReady()
+            }
+          }
+        }
+
+        return [toReady, toNotReady]
+      }
+
+      createdReadiness = Array.from(Array(+n), () => {
+        const [toReady, toNotReady] = createDeduct()
+        const fnName = () => {
+          toReady()
+          return toNotReady
+        }
+        fnName.toNotReady = toNotReady
+        return fnName
+      })
+      return createdReadiness
+    },
+  }
+
+  return wrapLightship
+}
+
+export const createLightship = create
+
+export default create
